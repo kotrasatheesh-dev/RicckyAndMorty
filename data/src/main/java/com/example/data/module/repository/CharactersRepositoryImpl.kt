f823ad5e6996b1ba932611b217503269900b56fa
kotrasatheesh-dev <ksatheesh@griddynamics.com>
2025-01-03 18:06:55 +0530

    Added Multi Module data layer required class

--- data/src/main/java/com/example/data/module/repository/CharactersRepositoryImpl.kt
+++ data/src/main/java/com/example/data/module/repository/CharactersRepositoryImpl.kt
@@ -3,19 +3,7 @@ package com.example.data.module.repository
 import com.apollographql.apollo.ApolloClient
 import com.example.data.GetCharactersQuery
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class CharactersRepositoryImpl
-@Inject
-constructor(
-    val apolloClient: ApolloClient,
-) : CharactersRepository {
-
-    override suspend fun getCharacters(): Flow<List<GetCharactersQuery.Result>> =
-        flow {
-            val response = apolloClient.query(GetCharactersQuery()).execute()
-            val characters = response.data?.characters?.results?.filterNotNull().orEmpty()
-            emit(characters)
-        }
+class CharactersRepositoryImpl{
 }
