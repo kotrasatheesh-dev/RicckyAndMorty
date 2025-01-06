4aeb8573fdc15d46217c3c9fec8fdf0053ede822
kotrasatheesh-dev <ksatheesh@griddynamics.com>
2025-01-06 14:55:39 +0530

    Added Multi Module data layer required class

--- app/src/main/java/com/rickyandmonty/di/MainActivity.kt
+++ app/src/main/java/com/rickyandmonty/di/MainActivity.kt
@@ -7,12 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import com.rickyandmonty.di.theme.RickAndMortyTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            RickAndMortyTheme {
             }
         }
     }
-
+}
