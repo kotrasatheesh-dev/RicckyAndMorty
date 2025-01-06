c501a0da5442d1c17df3b9074cce644cd772ed9f
kotrasatheesh-dev <ksatheesh@griddynamics.com>
2025-01-06 11:43:50 +0530

    Added Multi Module data layer required class

--- app/src/main/java/com/rickyandmonty/di/theme/Theme.kt
+++ app/src/main/java/com/rickyandmonty/di/theme/Theme.kt
@@ -9,15 +9,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import com.example.rickandmorty.ui.theme.BLACK
-import com.example.rickandmorty.ui.theme.LightPink
-import com.example.rickandmorty.ui.theme.Pink40
-import com.example.rickandmorty.ui.theme.Pink80
-import com.example.rickandmorty.ui.theme.Purple40
-import com.example.rickandmorty.ui.theme.Purple80
-import com.example.rickandmorty.ui.theme.PurpleGrey40
-import com.example.rickandmorty.ui.theme.PurpleGrey80
-import com.example.rickandmorty.ui.theme.Typography
 
 private val DarkColorScheme =
     darkColorScheme(
