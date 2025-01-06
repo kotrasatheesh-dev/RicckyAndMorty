54375b95fcf3b48847934e46fb6c30e5c54c94ec
kotrasatheesh-dev <ksatheesh@griddynamics.com>
2025-01-06 13:17:28 +0530

    Added Multi Module data layer required class

--- app/src/main/java/com/rickyandmonty/di/MainActivity.kt
+++ app/src/main/java/com/rickyandmonty/di/MainActivity.kt
@@ -4,29 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.rickyandmonty.di.theme.RickAndMortyTheme
-
-// Ensure the correct theme name here
+import com.rickyandmonty.di.theme.rickAndMortyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RickAndMortyTheme   {  // Corrected theme name
+            rickAndMortyTheme   {
 
                 }
             }
         }
     }
 
-@Composable
-fun MyGreeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
