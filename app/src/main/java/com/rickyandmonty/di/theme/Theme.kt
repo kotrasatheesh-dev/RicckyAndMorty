b00da61935e9fcad5212030d37ed1d0911b62ef0
kotrasatheesh-dev <ksatheesh@griddynamics.com>
2025-01-06 16:11:13 +0530

    Added Multi Module data layer required class

--- app/src/main/java/com/rickyandmonty/di/theme/Theme.kt
+++ app/src/main/java/com/rickyandmonty/di/theme/Theme.kt
@@ -13,20 +13,20 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40,
+    tertiary = Pink40
 )
 
 @Composable
-fun RickAndMortyTheme(
+fun rickAndMortyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = true, // Dynamic color is available on Android 12+
-    content: @Composable () -> Unit,
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -44,6 +44,7 @@ fun RickAndMortyTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content,
+        content = content
     )
 }
+
