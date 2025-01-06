6adc67ccb832e7b75d4cd35cd09b2ba67c7c6ef8
kotrasatheesh-dev <ksatheesh@griddynamics.com>
2025-01-06 11:29:37 +0530

    Added Multi Module data layer required class

--- app/src/main/java/com/rickyandmonty/di/MainActivity.kt
+++ app/src/main/java/com/rickyandmonty/di/MainActivity.kt
@@ -4,21 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.rickandmorty.presentation.CharactersViewModel
-import com.example.rickandmorty.presentation.HomesScreen
 import com.example.rickandmorty.ui.theme.RickAndMortyTheme
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
-    @Inject lateinit var viewModel: CharactersViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        (application as RickAndMortyApplication).appComponent.inject(this)
         enableEdgeToEdge()
         setContent {
             RickAndMortyTheme {
-                HomesScreen()
+
             }
         }
     }
