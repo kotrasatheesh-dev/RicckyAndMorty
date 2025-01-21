33a300e30829196f3b5f7092521e0e3cdf4b1af3
kotrasatheesh-dev <ksatheesh@griddynamics.com>
2025-01-21 15:04:22 +0530

    Fix:some changes made

--- presentation/src/main/java/com/example/presentation/navigation/characters/AllCharacters.kt
+++ presentation/src/main/java/com/example/presentation/navigation/characters/AllCharacters.kt
@@ -1,41 +1,35 @@
 package com.example.presentation.navigation.characters
 
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.presentation.navigation.uistate.UiState
-
+import androidx.compose.material3.CircularProgressIndicator
 @Composable
 fun AllCharacters(
-    uiState: UiState<List<GetCharactersQuery.Result>>,
+    uiState: UiState,
     onLoadCharacters: () -> Unit,
     modifier: Modifier = Modifier,
-    onNavigateToCharacterDetails: (String) -> Unit,
 ) {
     when (uiState) {
         is UiState.Empty -> {
             onLoadCharacters()
         }
         is UiState.Error -> {
-            // Display an error message or retry button
+            // Handle error state
         }
         is UiState.Loading -> {
             CircularProgressIndicator(modifier = modifier.size(100.dp))
         }
         is UiState.Success -> {
-            val charactersList = uiState.data
+            val charactersList = uiState.data // Assuming this is a List<GetCharactersQuery.Result>
             CharactersList(
-                charactersList = charactersList,
-                onNavigateToCharacterDetails = onNavigateToCharacterDetails,
-                modifier = modifier
+                charactersList = charactersList // Pass the list directly
             )
         }
         else -> Unit
     }
-}
-
 }
 
 
