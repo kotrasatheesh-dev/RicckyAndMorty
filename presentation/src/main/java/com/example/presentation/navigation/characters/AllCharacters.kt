cb4efde2f53c60bd749f401a4de43f12e36b2775
kotrasatheesh-dev <ksatheesh@griddynamics.com>
2025-01-22 15:27:26 +0530

    Fix:files some issues fixed

---
+++ presentation/src/main/java/com/example/presentation/navigation/characters/AllCharacters.kt
@@ -0,0 +1,38 @@
+package com.example.presentation.navigation.characters
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.presentation.navigation.uistate.UiState
+
+@Composable
+fun AllCharacters(
+    uiState: UiState,
+    onLoadCharacters: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    when (uiState) {
+        is UiState.Empty -> {
+            onLoadCharacters()
+        }
+        is UiState.Error -> {
+            // Handle error state
+        }
+        is UiState.Loading -> {
+            CircularProgressIndicator(modifier = modifier.size(100.dp))
+        }
+        is UiState.Success -> {
+            val charactersList = uiState.data // Assuming this is a List<GetCharactersQuery.Result>
+            CharactersList(
+                charactersList = charactersList // Pass the list directly
+            )
+        }
+        else -> Unit
+    }
+}
+
+
+
+
