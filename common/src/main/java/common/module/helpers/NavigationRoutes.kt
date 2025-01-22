024d05f3712f2c23daabc301cfa069cc3b01424a
kotrasatheesh-dev <ksatheesh@griddynamics.com>
2025-01-22 16:10:07 +0530

    Fix:files some issues fixed

--- common/src/main/java/common/module/helpers/NavigationRoutes.kt
+++ common/src/main/java/common/module/helpers/NavigationRoutes.kt
@@ -1,9 +1,5 @@
 package common.module.helpers
 
-sealed class NavigationRoutes(val route: String) {
-    data object AllCharacters : NavigationRoutes("AllCharacters")
-    data object CharacterDetails : NavigationRoutes("CharacterDetails/{CharacterId}") {
-        fun createRoute(characterId: String) = "CharacterDetails/$characterId"
-    }
-
+enum class NavigationRoutes {
+    AllCharacters,
 }
